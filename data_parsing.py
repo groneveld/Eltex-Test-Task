bbfb59ecefe80bfe9fb16dcdb343e8e25fda6c7c
groneveld <groneveld.co@gmail.com>
2020-09-08 23:54:06 +0700

    Route table as dictionary representation

--- data_parsing.py
+++ data_parsing.py
@@ -1,3 +1,23 @@
+import json
+
+
+def blocks_to_dictionary(block, dictionary):
+    destination = block[0][0]
+    preference = block[0][1]
+    age = block[0][2]
+    metric = block[0][3]
+    sub_dictionary = {'preference': preference, 'metric': metric, 'age': age, 'via': None}
+    for i in range(1, len(block)):
+        hop = block[i][0]
+        via = block[i][1]
+        sub_dictionary['via'] = via
+        if hop not in dictionary.keys():
+            dictionary[hop] = {destination: sub_dictionary}
+        else:
+            dictionary[hop][destination] = sub_dictionary
+    return dictionary
+
+
 def parse_by_markers(markers, line):
     start_index = 0
     result = []
@@ -16,14 +36,22 @@ def parse_by_markers(markers, line):
 
 if __name__ == '__main__':
     route_table = {'route_table': {'next_hop': {}}}
+    block = []
+    sub_dictionary = {}
     with open('dump.log') as reader:
         for line in reader:
             line = line.replace('\n', '')
             if line[0] != ' ':
+                new_block = True
                 markers = [('', ' '), ('/', ']'), (' ', ','), ('c ', '')]
                 result = parse_by_markers(markers, line)
-                print(result)
             else:
+                new_block = False
                 markers = [('to ', ' '), ('via ', '')]
                 result = parse_by_markers(markers, line)
-                print(result)
+            if new_block:
+                if len(block) > 0:
+                    sub_dictionary = blocks_to_dictionary(block, sub_dictionary)
+                block.clear()
+            block.append(result)
+    route_table['route_table']['next_hop'] = sub_dictionary
