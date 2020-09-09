cfac247da921ba11c98e84869bc4e02a9f2cb709
groneveld <groneveld.co@gmail.com>
2020-09-09 20:39:27 +0700

    DB writing and reading

--- database_importing.py
+++ database_importing.py
@@ -3,6 +3,18 @@ import sqlite3
 import sys
 from sqlite3 import Error
 
+
+def print_db(data):
+    string = "|{0:^20}|{1:^10}|{2:^10}|{3:^20}|{4:^15}|{5:^15}|"
+    print("_" * 97)
+    print(string.format("Destination", "Preference", "Metric", "Next Hop", "Interface", "Age"))
+    print("-" * 97)
+    prev = ""
+    for line in data:
+        print(string.format(line[1] if line[1] != prev else "", line[2], line[3], line[0], line[5], line[4]))
+        prev = line[1]
+
+
 if __name__ == '__main__':
     database_name = ""
     json_file_name = ""
@@ -21,6 +33,9 @@ if __name__ == '__main__':
         json_file = open(json_file_name)
         data = json.load(json_file)
         cursor = connection.cursor()
+        cursor.execute("DROP TABLE IF EXISTS next_hop")
+        cursor.execute("DROP TABLE IF EXISTS destination")
+        cursor.execute("DROP TABLE IF EXISTS hop_to_destination")
         cursor.execute("""CREATE TABLE IF NOT EXISTS next_hop
                           (hop_id INTEGER PRIMARY KEY, 
                           hop text)
@@ -56,8 +71,9 @@ if __name__ == '__main__':
             destination.destination, destination.preference, destination.metric, destination.age, destination.interface
             from hop_to_destination 
             inner join destination on destination.dest_id = hop_to_destination.dest_id 
-            inner join next_hop on next_hop.hop_id = hop_to_destination.hop_id 
+            inner join next_hop on next_hop.hop_id = hop_to_destination.hop_id
+            group by hop_to_destination.id 
             order by destination.destination""")
         table = cursor.fetchall()
-
+        print_db(table)
         connection.close()
